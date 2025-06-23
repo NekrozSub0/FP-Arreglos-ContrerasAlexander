602b4576e6c74a6bee92495cbca4cbec1651d846
Alexander <alex-c-r@hotmail.es>
2025-06-23 12:35:16 -0500

    Ejercicio 14 - Unir 2 cadenas, versión final

---
+++ Ejercicio 14.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void mistrcat(char str1[], const char str2[]) {
+    int i = 0;
+    while (str1[i] != '\0') {
+        i++;
+    }
+    int j = 0;
+    while (str2[j] != '\0') {
+        str1[i] = str2[j];
+        i++;
+        j++;
+    }
+    str1[i] = '\0';
+}
+
+int main() {
+    char str1[50];
+    char str2[50];
+
+    cout << "Ingrese la cadena base (str1): ";
+    cin.getline(str1, 50);
+
+    cout << "Ingrese la cadena para concatenar (str2): ";
+    cin.getline(str2, 50);
+
+    mistrcat(str1, str2);
+
+    cout << "str1 despues de concatenar: " << str1 << endl;
+
+    return 0;
+}
