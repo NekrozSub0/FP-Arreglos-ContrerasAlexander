63e1a81ca0cd7d8a65206c8cd1c8f9fe174b3028
Alexander Contreras <alex-c-r@hotmail.es>
2026-01-16 23:15:48 -0500

    Agregado de subtitulos

--- Ejercicio 13.cpp
+++ Ejercicio 13.cpp
@@ -11,10 +11,14 @@ void mistrcpy(char str1[], const char str2[]) {
 }
 
 int main(){
+	//Declaracion de variables
 	char str1[30], str2[30];
+	//Entrada
 	cout<<"Ingrese el arreglo"<<endl;
 	cin.getline(str2,30);
+	//Proceso
 	mistrcpy(str1, str2);
+	//Salida
 	cout<<"Arreglo copiado: "<<str2<<endl;
 	cout<<"Arreglo original: "<<str1<<endl;
 	return 0;
